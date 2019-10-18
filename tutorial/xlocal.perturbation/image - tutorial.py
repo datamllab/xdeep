69b5a300e096d74071b86fe9c6a6e9bca60f748d
Fan Yang <nacoyang@gmail.com>
2019-10-18 15:14:36 -0500

    refactor the turtorial codes of perturbation

---
+++ tutorial/xlocal.perturbation/image - tutorial.py
@@ -0,0 +1,73 @@
+# Please set the folder slim according to your own path.
+# Download slim at https://github.com/tensorflow/models/tree/master/research/slim
+import sys
+sys.path.append("./slim")
+import os
+import tensorflow as tf
+from nets import inception
+from preprocessing import inception_preprocessing
+from datasets import imagenet
+from skimage.segmentation import slic
+import xdeep.xlocal.perturbation.xdeep_image as xdeep_image
+
+def transform_img_fn(path_list):
+    out = []
+    for f in path_list:
+        with open(f,'rb') as img:
+            image_raw = tf.image.decode_jpeg(img.read(), channels=3)
+            image = inception_preprocessing.preprocess_image(image_raw, image_size, image_size, is_training=False)
+            out.append(image)
+    return session.run([out])[0]
+
+def test_image_data():
+    slim = tf.contrib.slim
+    tf.reset_default_graph()
+    session = tf.Session()
+
+    image_size = inception.inception_v3.default_image_size
+
+    names = imagenet.create_readable_names_for_imagenet_labels()
+
+    processed_images = tf.placeholder(tf.float32, shape=(None, 299, 299, 3))
+
+    with slim.arg_scope(inception.inception_v3_arg_scope()):
+        logits, _ = inception.inception_v3(processed_images, num_classes=1001, is_training=False)
+    probabilities = tf.nn.softmax(logits)
+
+    # Please correctly set the model path.
+    # Download the model at https://github.com/tensorflow/models/tree/master/research/slim
+    checkpoints_dir = 'model'
+    init_fn = slim.assign_from_checkpoint_fn(
+        os.path.join(checkpoints_dir, 'inception_v3.ckpt'),
+        slim.get_model_variables('InceptionV3'))
+    init_fn(session)
+
+    def predict_fn(images):
+        return session.run(probabilities, feed_dict={processed_images: images})
+
+    def f(x):
+        return x / 2 + 0.5
+
+    class_names = []
+    for item in names:
+        class_names.append(names[item])
+
+    images = transform_img_fn(['data/violin.JPEG'])
+    image = images[0]
+
+    explainer = xdeep_image.ImageExplainer(predict_fn, class_names)
+
+    explainer.explain('lime', image, top_labels=3)
+    explainer.show_explanation('lime', deprocess=f, positive_only=False)
+
+    explainer.explain('cle', image, top_labels=3)
+    explainer.show_explanation('cle', deprocess=f, positive_only=False)
+
+    explainer.explain('anchor', image)
+    explainer.show_explanation('anchor')
+
+    segments_slic = slic(image, n_segments=50, compactness=30, sigma=3)
+    explainer.initialize_shap(n_segment=50, segment=segments_slic)
+    explainer.explain('shap',image,nsamples=400)
+    explainer.show_explanation('shap',deprocess=f)
+
---
+++ tutorial/xlocal.perturbation/tabular - tutorial.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from anchor import utils
+import xdeep.xlocal.perturbation.xdeep_tabular as xdeep_tabular
+
+# Please download the dataset at
+# https://archive.ics.uci.edu/ml/datasets/adult
+# Then reset the path.
+
+def test_tabular_data():
+    dataset_folder = 'data/'
+    dataset = utils.load_dataset('adult', balance=True, dataset_folder=dataset_folder)
+
+    c = RandomForestClassifier(n_estimators=50, n_jobs=5)
+    c.fit(dataset.train, dataset.labels_train)
+
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+
+    explainer = xdeep_tabular.TabularExplainer(c.predict_proba, ['<=50K', '>50K'], dataset.feature_names, dataset.train[0:50],
+                                               categorical_features=dataset.categorical_features, categorical_names=dataset.categorical_names)
+
+    explainer.set_parameters('lime', discretize_continuous=True)
+    explainer.explain('lime', dataset.test[0])
+    explainer.show_explanation('lime')
+
+    explainer.set_parameters('cle', discretize_continuous=True)
+    explainer.explain('cle', dataset.test[0])
+    explainer.show_explanation('cle')
+
+    explainer.explain('shap', dataset.test[0])
+    explainer.show_explanation('shap')
+
+    explainer.initialize_anchor(dataset.data)
+    encoder = explainer.get_anchor_encoder(dataset.train[0:50], dataset.labels_train, dataset.validation, dataset.labels_validation)
+    c_new = RandomForestClassifier(n_estimators=50, n_jobs=5)
+    c_new.fit(encoder.transform(dataset.train), dataset.labels_train)
+    explainer.set_anchor_predict_proba(c_new.predict_proba)
+    explainer.explain('anchor', dataset.test[0])
+    explainer.show_explanation('anchor')
+
