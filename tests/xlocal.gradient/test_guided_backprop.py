fb38c2accc44cafd5f1a6f43b7ee4a402c053a2f
Fan Yang <nacoyang@gmail.com>
2019-11-14 11:48:13 -0600

    test formatting

---
+++ tests/xlocal.gradient/test_guided_backprop.py
@@ -0,0 +1,17 @@
+import torchvision.models as models
+
+from xdeep.utils import *
+from xdeep.xlocal.gradient.backprop.guided_backprop import *
+
+
+def test_bp():
+
+  image_path = 'images/ILSVRC2012_val_00000073.JPEG'
+  image_name = image_path.split('/')[1]
+  image = load_image(image_path)
+  norm_image = apply_transforms(image)
+
+  model = models.vgg16(pretrained=True)
+  Guided_BP = Backprop(model, guided=True)
+  output = Guided_BP.calculate_gradients(norm_image)
+  visualize(norm_image, output, save_path='results/GuidedBP_' + image_name)
