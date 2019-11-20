f0c2e92bcdcd6b3284b7aea715b78cd6c988511c
Fan Yang <nacoyang@gmail.com>
2019-11-19 22:02:00 -0600

    update xlocal.gradient test

--- tests/xlocal.gradient/test_guided_backprop.py
+++ tests/xlocal.gradient/test_guided_backprop.py
@@ -6,7 +6,7 @@ from xdeep.xlocal.gradient.backprop.guided_backprop import *
 
 def test_bp():
 
-  image_path = './xlocal.gradient/images/ILSVRC2012_val_00000073.JPEG'
+  image_path = 'tests/xlocal.gradient/images/ILSVRC2012_val_00000073.JPEG'
   image_name = image_path.split('/')[1]
   image = load_image(image_path)
   norm_image = apply_transforms(image)
@@ -14,4 +14,4 @@ def test_bp():
   model = models.vgg16(pretrained=True)
   Guided_BP = Backprop(model, guided=True)
   output = Guided_BP.calculate_gradients(norm_image)
-  visualize(norm_image, output, save_path='./xlocal.gradient/results/GuidedBP_' + image_name)
+  visualize(norm_image, output, save_path='tests/xlocal.gradient/results/GuidedBP_' + image_name)
